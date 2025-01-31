64199e4c995d3836c7f5b23ccc3a6baec046d198
Alexander Chernyaev <46301523+HyTekCoop@users.noreply.github.com>
2025-01-31 21:00:50 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -2,14 +2,17 @@ from g4f.client import Client
 from fastapi import FastAPI
 from pydantic import BaseModel
 import re
-
+from typing import List
+from duckduckgo_search import DDGS  
 
 app = FastAPI()
 
+#Описание класса вопроса на вход
 class QueryRequest(BaseModel):
     query: str
     id: int
 
+#Запрос на овет от openAI
 def ask_openai(query: str) -> str:
     client = Client()
     response = client.chat.completions.create(
@@ -19,7 +22,13 @@ def ask_openai(query: str) -> str:
     )
     return response.choices[0].message.content
 
+#Предаставление ссылок на информацию, не больше 2
+def search_duckduckgo(query: str) -> List[str]:
+    with DDGS() as ddgs:
+        results = ddgs.text(query, max_results=2)  
+    return [r["href"] for r in results if "itmo" in r["href"]]  
 
+#Серверная часть для запроса через POST
 @app.post("/api/request")
 async def handle_request(request: QueryRequest):
     query = request.query
@@ -28,25 +37,24 @@ async def handle_request(request: QueryRequest):
     match = re.findall(r"\d+\.\s(.+?)(?:\n|$)", query)
     options = match if match else None
 
-    response = ask_openai(query)
+    response_text = ask_openai(query)
+    sources = search_duckduckgo(f"site:itmo.ru {query}")  
 
     selected_answer = None
     if options:
         for i, option in enumerate(options, start=1):
-            if option.lower() in response.lower():
+            if option.lower() in response_text.lower():
                 selected_answer = i
                 break
 
     response = {
         "id": query_id,
         "answer": selected_answer,
-        "reasoning": response,
-        "sources": "test"
+        "reasoning": response_text,
+        "sources": sources 
     }
 
     return response
-
-
 
 # Команда для запуска: uvicorn main:app --reload
 if __name__ == "__main__":
